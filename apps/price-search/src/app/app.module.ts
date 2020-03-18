4820255e4538539cc382779dda710daf637dfdf1
igmelo <igorgalvao@gec.inatel.br>
2020-03-18 18:46:22 +0000

    feat(implementation of toolbar): homepae toolbar

--- apps/price-search/src/app/app.module.ts
+++ apps/price-search/src/app/app.module.ts
@@ -1,4 +1,8 @@
 import { Module } from '@nestjs/common';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 import {
   SocialLoginModule,
   AuthServiceConfig,
@@ -8,6 +12,7 @@ import {
 import { BrowserModule } from '@angular/platform-browser';
 import { PriceSearchComponent } from './app.component';
 import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const google_oauth_client_id =
   '113929152064-osdmb61gpl4d06ls9717kasgfntc4dam.apps.googleusercontent.com';
@@ -21,7 +26,14 @@ const config = new AuthServiceConfig([
 
 @NgModule({
   declarations: [PriceSearchComponent],
-  imports: [BrowserModule, SocialLoginModule.initialize(config)],
+  imports: [
+    BrowserModule,
+    MatButtonModule,
+    SocialLoginModule.initialize(config),
+    MatToolbarModule,
+    MatIconModule,
+    BrowserAnimationsModule
+  ],
   providers: [],
   bootstrap: [PriceSearchComponent]
 })
